9fa0e75975d371e1f4d96660d02e60ad605dbb53
mamt4real <mamt4real@gmail.com>
2022-09-20 11:15:12 -0700

    last tax

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-
-def uppercase(s):
-    """prints a string in uppercase"""
-    lower = range(97, 97 + 26)
-    for c in s:
-        i = ord(c)
-        if i in lower:
-            i -= 32
-        print("{n:c}".format(n=i), end='')
-    print()
-
-
-if __name__ == '__main__':
-    uppercase('some856lowercase₦&@')
+def uppercase(str):
+    print("".join(["{:c}".format(ord(c)-32)
+          if ord(c) in range(97, 123) else c
+          for c in str]))
