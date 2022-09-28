387a0f8e5a25cbd1a11c623e3d8dac4834460cb5
mamt4real <mamt4real@gmail.com>
2022-09-28 11:33:43 -0700

    Debug 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,24 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    """
-    Square each element of matrix
-    Args:
-        matrix - a 2d lists
-    """
-    if matrix is None:
-        return None
-    return list(
-        map(
-            lambda row: list(map(lambda x: x**2, row)), matrix
-        )
-    )
-
-
-if __name__ == '__main__':
-    s = square_matrix_map([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ])
-    print(s)
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
