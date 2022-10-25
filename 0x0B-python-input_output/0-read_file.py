558cbb4facd2db1a003717898f7f68a93e5d7666
mamt4real <mamt4real@gmail.com>
2022-10-25 06:44:23 -0700

    All tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            print(f.read(), end="")
+    except FileNotFoundError:
+        print("File not found!")
+        return
--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Here we will exerience the perks that came with modern HLLs whilst reminiscing t
 * [0x06-python-classes](0x06-python-classes)
 * [0x07-python-test_driven_development](0x07-python-test_driven_development)
 * [0x0A-python-inheritance](0x0A-python-inheritance)
+* [0x0B-python-input_output](0x0B-python-input_output)
